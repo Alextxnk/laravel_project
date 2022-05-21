7bff453449e1946b8c1ff4a08f2c1958ea966421
Alextxnk <alexsolov766@gmail.com>
2022-05-21 20:24:31 +0300

    test

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+ini_set('memory_limit', '-1');
 class User extends Authenticatable
 {
     use Notifiable;
